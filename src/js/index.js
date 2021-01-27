b7b3bf28af7c90e1f25fc01684785e0e172dfc14
Anna Udziela <annaudziela@gmail.com>
2021-01-27 15:16:34 +0100

    adding a counter refresh function and shortening the JS code

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,12 @@ const counterField = document.querySelector('.counter--js');
 const addButton = document.querySelector('.button__add--js');
 const lessButton = document.querySelector('.button__less--js');
 const key = new Date().toLocaleString().slice(0, 10);
-
 let currentValue = 0; 
-
 const localStorageValue = localStorage.getItem(key);
+const updateCounter = () => {
+    counterField.innerHTML = currentValue;
+    localStorage.setItem(key, currentValue);
+} 
 
 if (localStorageValue) {
     currentValue = localStorageValue;
@@ -21,26 +23,23 @@ counterField.innerHTML = currentValue;
 
 addButton.addEventListener('click', () => {
     if (currentValue < 99) {
-    currentValue++; }
-    counterField.innerHTML = currentValue;
-    localStorage.setItem(key, currentValue);
+    currentValue++; 
+    updateCounter();
+
+}
+if (currentValue > 98) {
+    alert("Seems you are well hydrated");
+}
+   
 });
 
 lessButton.addEventListener('click', () => {
     if (currentValue > 0) {
         currentValue--;
+        updateCounter();
     }
-        counterField.innerHTML = currentValue;
-        localStorage.setItem(key, currentValue);
 
 });
-
-addButton.addEventListener('click', () => {
-if (currentValue > 98) {
-    alert("Seems you are well hydrated");
-}
-});
-
 
 
 
